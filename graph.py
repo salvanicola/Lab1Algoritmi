81685dea5f3083b33d1510824c5642a9940217b0
TheDoctor200 <alex.thedoctor200@gmail.com>
2021-04-10 19:10:21 +0200

    Commenti freschi

--- graph.py
+++ graph.py
@@ -1,46 +1,51 @@
 class Arc:
+    # Funzione di inizializzazione dell'arco.
     def __init__(self, v1, v2, w):
         self.vert1 = v1
         self.vert2 = v2
         self.weight = w
 
+    # Funzione di accesso al peso dell'arco.
     def weight(self):
         return self.weight
 
 
 class Graph:
+    # Funzione di inizializzazione del grafo.
     def __init__(self, v, a):
         self.list = []
         self.n_vertexes = v
         self.n_arches = a
 
+    # Funzione di aggiunta di un arco al grafo.
     def add(self, v1, v2, w):
         self.list.append(Arc(v1, v2, w))
 
+    # Funzione di rimozione di un arco dal grafo.
     def remove(self, arch):
         self.list.remove(arch)
 
-
-def graph_generator(file):
-    try:
-        fp = open(file, "r")
-        lines = fp.readlines()
-        if lines.__len__() > 0:
-            parsing = lines[0].split(" ")
-            graph = Graph(int(parsing[0]), int(parsing[1]))
-            lines.pop(0)
-            for x in lines:
-                x = x.split(" ")
-                graph.add(int(x[0])-1, int(x[1])-1, int(x[2]))
-    finally:
-        fp.close()
-        return graph
-
+    # Funzione di generazione di un grafo a partire da un file fornito in input.
+    def graph_generator(file):
+        try:
+            fp = open(file, "r")
+            lines = fp.readlines()
+            if lines.__len__() > 0:
+                parsing = lines[0].split(" ")
+                graph = Graph(int(parsing[0]), int(parsing[1]))
+                lines.pop(0)
+                for x in lines:
+                    x = x.split(" ")
+                    graph.add(int(x[0])-1, int(x[1])-1, int(x[2]))
+        finally:
+            fp.close()
+            return graph
 
 class Vertex:
+    # Funzione di inizializzazione di un vertice.
     def __init__(self, i, k=float('inf')):
         self.key = k
         self.parent = None
         self.id = i
-        # flag dice se è già stato estratto dallo spanning tree
+        # Flag che indica se il vertice è già stato estratto dallo spanning tree.
         self.flag = False
--- prim.py
+++ prim.py
@@ -1,22 +1,22 @@
 import logging
-
 import progressbar
-
 from heap import FibonacciHeap, Node
 from graph import Vertex
 import time
 from progress.bar import Bar
 
 def prim(G, s):
+    # Inizializzazione di una semplice progress bar per verificare l'operazione in corso.
     with progressbar.ProgressBar(G.n_vertexes) as bar:
         logger = logging.getLogger('tipper')
         logger.debug("ESEGUO prim su grafo con %s vertici e %s archi", G.n_vertexes, G.n_arches)
         vert = []
         nodes = []
         q = FibonacciHeap()
-        # assumo che tutti i valori da 0 a n_vertexes - 1 (numero di vertici del grafo) siano tutti gli esatti indici
-        # dei vertici
+        # Assumiamo che tutti i valori da 0 a n_vertexes - 1 (numero di vertici del grafo) siano tutti gli esatti indici
+        # dei vertici.
 
+        # Inizializza gli array ed il Fibonacci Heap con dei valori di base.
         for x in range(0, G.n_vertexes):
             v = Vertex(x)
             if x == s:
@@ -28,18 +28,25 @@ def prim(G, s):
 
         initial_node_number = 0
 
+        # Viene iterato un ciclo while, fino a che non vengono esauriti tutti gli elementi del Fibonacci Heap.
         while q.total_num_elements > 0:
+            # u_id è l'indice di un light edge per l'algoritmo.
             u_id = q.extract_minimum()
             vert[u_id].flag = True
             v_vert = None
+            # Vengono esaminati tutti i vertici adiacenti al vertice identificato da u_id.
             for v_arch in G.list:
                 if v_arch.vert1 == u_id:
                     v_vert = v_arch.vert2
                 elif v_arch.vert2 == u_id:
                     v_vert = v_arch.vert1
                 if v_vert is not None and vert[v_vert].flag is False and v_arch.weight < vert[v_vert].key:
-                    vert[v_vert].parent = nodes[u_id] # questo mi preoccupa
+                    # Se verificate le condizioni, il vertice viene inserito nel corretto indice della lista vert,
+                    # diventando componente del MST da restituire a fine ciclo.
+                    vert[v_vert].parent = nodes[u_id]
                     vert[v_vert].key = v_arch.weight
+                    # Viene aggiornata la chiave del vertice all'interno del Fibonacci Heap con il valore dell'arco
+                    # preso in esame.
                     q.decrease_key(nodes[v_vert], v_arch.weight)
             initial_node_number = initial_node_number + 1
             bar.update(initial_node_number)
