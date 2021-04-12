f6a4132dec58da688cd50897a750e4e3df24be35
TheDoctor200 <alex.thedoctor200@gmail.com>
2021-04-12 10:13:29 +0200

    Fix DFS Merge

--- dfs.py
+++ dfs.py
@@ -1,20 +1,27 @@
 def dfs(graph):
     for v in graph.vert_list:
-        if v.flag is False:
-            if dfs_rec(graph, v) is True:
-                return True
+        if v is not None:
+            if v.flag is False:
+                if dfs_rec(graph, v) is True:
+                    return True
     return False
 
 
 def dfs_rec(graph, vert):
-    if not vert.flag:
-        vert.flag = True
-        for arch in graph.arch_list:
-            if arch.vert1 == vert:
-                return dfs_rec(graph, arch.vert2)
-            elif arch.vert2 == vert:
-                return dfs_rec(graph, arch.vert1)
-            else:
-                return False
-    else:
-        return True
+    vert.flag = True
+    for arch in graph.arch_list:
+        if arch.label is None:
+            opposite = None
+            if arch.vert1 == vert.id:
+                opposite = graph.vert_list[arch.vert2]
+            elif arch.vert2 == vert.id:
+                opposite = graph.vert_list[arch.vert1]
+            if opposite is not None:
+                if opposite.flag is False:
+                    arch.label = "discovery"
+                    if dfs_rec(graph, opposite) is True:
+                        return True
+                else:
+                    arch.label = "back"
+                    return True
+    return False
