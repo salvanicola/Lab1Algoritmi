c3519e97ba3e90981993e43a9ed6a12729b359d5
TheDoctor200 <alex.thedoctor200@gmail.com>
2021-04-13 15:57:47 +0200

    Correzione Kruskal

--- kruskal_naive.py
+++ kruskal_naive.py
@@ -16,6 +16,10 @@ def kruskalNaive(G):
         else:
             if dfs(A):
                 A.pop()
+            for x in A.arch_list:
+                x.label = None
+                A.vert_list[x.vert1].flag = False
+                A.vert_list[x.vert2].flag = False
     logger.debug("Kruskal su grafo con %s vertici e %s archi ha terminato", G.n_vertexes, G.n_arches)
     return A
 
