b29179d9076a6cf6a6ecf70ede4778bf057893b2
salvanicola <nicola.salvadore@gmail.com>
2021-04-13 13:00:01 +0200

    suca

--- main.py
+++ main.py
@@ -6,6 +6,8 @@
 
 # Press the green button in the gutter to run the script.
 import logging
+import multiprocessing
+from multiprocessing import Process
 
 from dfs import dfs
 from graph import graph_generator, Graph
@@ -20,13 +22,19 @@ from prim import prim
 ###################
 # mac fix
 import re
+
 numbers = re.compile(r'(\d+)')
 
+logger = logging.getLogger('tipper')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
 
 def numerical_sort(value):
     parts = numbers.split(value)
     parts[1::2] = map(int, parts[1::2])
     return parts
+
+
 ####################
 
 # ho dovuto ordinare i file per nome (ty stefano lavori)
@@ -38,41 +46,35 @@ def upload_graph():
     return graphs
 
 
+def exec_threading(graph):
+    # root = x.arch_list[0].vert1
+    start_time = time.time()
+    kruskalNaive(graph)
+    stop_time = time.time() - start_time
+    logger.debug("--- %s seconds ---" % stop_time)
+    return stop_time, graph.n_vertexes * graph.n_arches
+
+
 if __name__ == '__main__':
 
-    logger = logging.getLogger('tipper')
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(logging.StreamHandler())
-
     graphs = upload_graph()
-    # threads = list()
     index = 0
-
     times = []
-    complex = []
-    for x in graphs:
-        root = x.arch_list[0].vert1
-        # thread = threading.Thread(target=prim, args=(x, root))
-        # threads.append(thread)
-        # logger.debug("create and start thread number %s", index)
-        # thread.start()
-        start_time = time.time()
-        prim(x, root)
-        stop_time = time.time() - start_time
-        logger.debug("--- %s seconds ---" % stop_time)
-        # index = index + 1
-        times.append(stop_time)
-        complex.append(x.n_vertexes + x.n_arches + math.log(x.n_arches))
+    complexity = []
 
-    # for index, thread in enumerate(threads):
-    #     thread.join()
-    # logger.debug(" the end ")
-    plt.plot(complex, times)
+    p = multiprocessing.Pool(processes=graphs.__len__())
+    data = p.map(exec_threading, [x for x in graphs])
+
+    for x in data:
+        times.append(x[0])
+        complexity.append(x[1])
+
+    plt.plot(complexity, times)
     temp = 0
     for x in range(0, len(times)):
-        temp = temp + times[x] / complex[x]
+        temp = temp + times[x] / complexity[x]
     temp = temp / len(times)
-    plt.plot(complex, [x*temp for x in complex])
+    plt.plot(complexity, [x * temp for x in complexity])
     plt.ylabel('Operation time')
-    plt.xlabel('m + n * ln(n)')
+    plt.xlabel('m * n')
     plt.show()
