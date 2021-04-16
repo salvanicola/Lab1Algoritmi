e70782faa856049ffe5d160a78fb9adc9277892f
salvanicola <nicola.salvadore@gmail.com>
2021-04-16 15:23:55 +0200

    vietnam destroyed

--- kruskal_naive.py
+++ kruskal_naive.py
@@ -8,6 +8,7 @@ from merge_sort import mergeSort
 
 def kruskalNaive(G):
     logger = logging.getLogger('tipper')
+    logger.debug("KRUSKAL NAIVE for %s", G.n_vertexes)
     A = Graph(G.n_vertexes)
     mergeSort(G.arch_list)
     for e in G.arch_list:
@@ -16,10 +17,7 @@ def kruskalNaive(G):
             break
         else:
             if dfs(A, G.n_vertexes):
-                start_time = time.time()
                 A.pop()
-                stop_time = time.time() - start_time
-                logger.debug("La pop ci ha messo %s", stop_time)
     # logger.debug("Kruskal su grafo con %s vertici e %s archi ha terminato", G.n_vertexes, G.n_arches)
     return A
 
--- main.py
+++ main.py
@@ -5,7 +5,10 @@
 
 
 # Press the green button in the gutter to run the script.
+import gc
 import logging
+
+from numpy import average
 
 from dfs import dfs
 from graph import graph_generator, Graph
@@ -20,6 +23,7 @@ from prim import prim
 ###################
 # mac fix
 import re
+
 numbers = re.compile(r'(\d+)')
 
 
@@ -27,6 +31,8 @@ def numerical_sort(value):
     parts = numbers.split(value)
     parts[1::2] = map(int, parts[1::2])
     return parts
+
+
 ####################
 
 # ho dovuto ordinare i file per nome (ty stefano lavori)
@@ -38,41 +44,49 @@ def upload_graph():
     return graphs
 
 
-if __name__ == '__main__':
+def plotting_plot(times, complexity):
+    plt.plot(complexity, times)
+    temp = 0
+    for x in range(0, len(times)):
+        temp = temp + times[x] / complex[x]
+    temp = temp / len(times)
+    plt.plot(complexity, [x * temp for x in complexity])
+    plt.ylabel('Operation time')
+    plt.xlabel('m')
+    plt.show()
 
+
+def testing(graphs, function, **args):
+    times = []
+    complexity = []
+    current_instance = 0
+    instance_list = []
+    for x in graphs:
+        if x.n_vertexes != current_instance:
+            avg = average(instance_list)
+            logger.debug("--- %s seconds --- for %s", avg, current_instance)
+            times.append(average(instance_list))
+            complexity.append(current_instance)
+            instance_list = []
+        gc.disable()
+        start_time = time.perf_counter_ns()
+        function(x)
+        stop_time = time.perf_counter_ns()
+        gc.enable()
+        instance_list.append(stop_time - start_time)
+        current_instance = x.n_vertexes
+    avg = average(instance_list)
+    logger.debug("--- %s seconds --- for %s", avg, current_instance)
+    times.append(average(instance_list))
+    complexity.append(current_instance)
+    return times, complexity
+
+
+if __name__ == '__main__':
     logger = logging.getLogger('tipper')
     logger.setLevel(logging.DEBUG)
     logger.addHandler(logging.StreamHandler())
 
     graphs = upload_graph()
-    # threads = list()
-    index = 0
-
-    times = []
-    complex = []
-    for x in graphs:
-        root = x.arch_list[0].vert1
-        # thread = threading.Thread(target=prim, args=(x, root))
-        # threads.append(thread)
-        # logger.debug("create and start thread number %s", index)
-        # thread.start()
-        start_time = time.time()
-        prim(x, root)
-        stop_time = time.time() - start_time
-        logger.debug("--- %s seconds ---" % stop_time)
-        # index = index + 1
-        times.append(stop_time)
-        complex.append(x.n_vertexes + x.n_arches + math.log(x.n_arches))
-
-    # for index, thread in enumerate(threads):
-    #     thread.join()
-    # logger.debug(" the end ")
-    plt.plot(complex, times)
-    temp = 0
-    for x in range(0, len(times)):
-        temp = temp + times[x] / complex[x]
-    temp = temp / len(times)
-    plt.plot(complex, [x*temp for x in complex])
-    plt.ylabel('Operation time')
-    plt.xlabel('m + n * ln(n)')
-    plt.show()
+    times, complexity = testing(graphs, kruskalNaive)
+    plotting_plot(times, complexity)
